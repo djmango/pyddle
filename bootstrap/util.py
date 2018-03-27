12c0a715fb658a08f2ed9206309c74d9d27b2a92
Sulaiman Ghori <skghori03@gmail.com>
2018-03-27 03:38:14 -0400

    attempted tcp hole punching

---
+++ bootstrap/util.py
@@ -0,0 +1,60 @@
+""" general utility functions for the bootstrap system """
+
+# imports
+import struct
+from collections import namedtuple
+
+
+def addr_from_args(args, ip='127.0.0.1', port=9999):
+    """ convert arguments [ip, port] into an address (ip, port) """
+    if len(args) >= 3:
+        ip, port = args[1], int(args[2])
+    elif len(args) == 2:
+        ip, port = ip, int(args[1])
+    else:
+        ip, port = ip, port
+    return ip, port
+
+
+def msg_to_addr(data):
+    """ convert raw message (ip:port) into an address (ip, port) """
+    ip, port = data.decode('utf-8').strip().split(':')
+    return (ip, int(port))
+
+
+def addr_to_msg(addr):
+    """ convert address [ip, port] into a message (ip:port) """
+    return '{}:{}'.format(addr[0], str(addr[1])).encode('utf-8')
+
+
+def send_msg(sock, msg):
+    """ prefix each message with a 4-byte length (network byte order) """
+    msg = struct.pack('>I', len(msg)) + msg
+    sock.sendall(msg)
+
+
+def recvall(sock, n):
+    """ helper function to recv n bytes or return None if EOF is hit """
+    data = b''
+    while len(data) < n:
+        packet = sock.recv(n - len(data))
+        if not packet:
+            return None
+        data += packet
+    return data
+
+
+def recv_msg(sock):
+    """ read message length and unpack it into an integer """
+    raw_msglen = recvall(sock, 4)
+    if not raw_msglen:
+        return None
+    msglen = struct.unpack('>I', raw_msglen)[0]
+    # read the message data
+    return recvall(sock, msglen)
+
+
+class Client(namedtuple('Client', 'conn, pub, priv')):
+
+    def peer_msg(self):
+        return addr_to_msg(self.pub) + b'|' + addr_to_msg(self.priv)
